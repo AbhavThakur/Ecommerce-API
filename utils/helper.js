9b5b8aafc50ed20610b5f49bcc1256c56279f92a
Abhav Thakur <abhav.thakur2018@vitstudent.ac.in>
2023-09-29 00:27:57 +0530

    added logic to generate unique number

--- utils/helper.js
+++ utils/helper.js
@@ -11,13 +11,22 @@ export const Modal = (modalName, Schema) => mongoose.model(modalName, Schema);
 export const stripe = new Stripe(process.env.STRIPE_KEY);
 
 export function generateRandomOrderNumber(length) {
+  const generatedOrderNumbers = new Set();
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let result = '';
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters.charAt(randomIndex);
-  }
+  while (true) {
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
 
-  return result;
+    if (!generatedOrderNumbers.has(result)) {
+      generatedOrderNumbers.add(result);
+      return result;
+    }
+
+    // If the generated order number already exists, reset it and try again
+    result = '';
+  }
 }
