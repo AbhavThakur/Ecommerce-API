738f3e5bff6c4970d663ee23c5d9751f4072a048
Abhav Thakur <abhav.thakur2018@vitstudent.ac.in>
2023-09-29 23:51:22 +0530

    added validation  before storing coupons

--- model/CouponModal.js
+++ model/CouponModal.js
@@ -27,8 +27,41 @@ const CouponSchema = new Schema(
   },
   {
     timestamps: true,
+    toJSON: { virtuals: true },
   }
 );
+
+//coupon is expired
+CouponSchema.virtual('isExpired').get(function () {
+  return this.endDate < new Date();
+});
+
+// validations
+CouponSchema.pre('validate', function (next) {
+  if (this.endDate < this.startDate) {
+    next(new Error('End date must be after start date'));
+  }
+  next();
+});
+CouponSchema.pre('validate', function (next) {
+  if (this.startDate < Date.now()) {
+    next(new Error('Start date must greater than today date'));
+  }
+  next();
+});
+CouponSchema.pre('validate', function (next) {
+  if (this.endDate < Date.now()) {
+    next(new Error('End date must be greater than today date'));
+  }
+  next();
+});
+
+CouponSchema.pre('validate', function (next) {
+  if (this.discount < 0 || isNaN(this.discount) || this.discount > 100) {
+    next(new Error('Discount must be a positive number and less than 100'));
+  }
+  next();
+});
 
 const CouponModal = Modal('Coupon', CouponSchema);
 
