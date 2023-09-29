69c18ffdbe56b1fbf43ba9415e400570955ead54
Abhav Thakur <abhav.thakur2018@vitstudent.ac.in>
2023-09-30 00:26:34 +0530

    create controller for update , delete  coupons

--- controllers/couponControllers.js
+++ controllers/couponControllers.js
@@ -28,11 +28,60 @@ export const createCouponControllers = asyncHandler(async (req, res) => {
   });
 });
 
+// get all coupons
 export const getAllCouponsControllers = asyncHandler(async (req, res) => {
   const coupons = await CouponModal.find();
   res.json({
     success: true,
     message: 'Coupons found',
     data: coupons,
+  });
+});
+
+// get coupon by user id
+export const getCouponByIdControllers = asyncHandler(async (req, res) => {
+  const coupons = await CouponModal.findById(req.params.id);
+  res.json({
+    success: true,
+    message: 'Coupons found',
+    data: coupons,
+  });
+});
+
+// update coupon by coupon id
+export const updateCouponControllers = asyncHandler(async (req, res) => {
+  const coupon = await CouponModal.findByIdAndUpdate(
+    req.params.id,
+    {
+      code: req.body.code?.toUpperCase(),
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      discount: req.body.discount,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!coupon) {
+    throw new Error('Coupon not found');
+  }
+  res.json({
+    success: true,
+    message: 'Coupon updated',
+    data: coupon,
+  });
+});
+
+// delete coupon by coupon id
+
+export const deleteCouponControllers = asyncHandler(async (req, res) => {
+  const coupon = await CouponModal.findByIdAndDelete(req.params.id);
+  if (!coupon) {
+    throw new Error('Coupon not found');
+  }
+  res.json({
+    success: true,
+    message: 'Coupon deleted',
+    data: coupon,
   });
 });
--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import {
   createCouponControllers,
+  deleteCouponControllers,
   getAllCouponsControllers,
+  getCouponByIdControllers,
+  updateCouponControllers,
 } from '../controllers/couponControllers.js';
 import { isloggedIn } from '../middlewares/isLoggedIn.js';
 
@@ -9,5 +12,8 @@ const couponRouter = express.Router();
 
 couponRouter.post('/', isloggedIn, createCouponControllers);
 couponRouter.get('/', getAllCouponsControllers);
+couponRouter.get('/:id', getCouponByIdControllers);
+couponRouter.put('/update/:id', isloggedIn, updateCouponControllers);
+couponRouter.delete('/delete/:id', isloggedIn, deleteCouponControllers);
 
 export default couponRouter;
